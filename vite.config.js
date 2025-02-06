dd63a542f8d84ea203d6ed8f3f12d9632e4e3a29
vinit <vinit.vala.9@gmail.com>
2025-02-06 14:50:47 -0500

    Next commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FIFA-Portfolio/', // This should match your GitHub repository name
+  base: '/FIFA-Portfolio/', // Make sure this matches your repo name EXACTLY
 });
